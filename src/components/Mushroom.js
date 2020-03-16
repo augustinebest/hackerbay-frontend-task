b95fc6dfa4192222d0bc90013689620f98d7609d
Igwe Augustine <augustusbest00@gmail.com>
2020-03-16 05:44:31 -0700

    refactoring on this files

---
+++ src/components/Mushroom.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import mushroom from '../assets/images/mushroom.svg';
+
+function Mushroom() {
+  return (
+    <img className="mushroom" src={mushroom} alt="mushroom" />
+  );
+}
+
+export default Mushroom;
